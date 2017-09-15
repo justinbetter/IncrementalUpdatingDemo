bea24d176fb5088f12d87e82a564f1484bf090cb
justin <hj@aituyou.com>
2017-09-15 14:52:05 +0800

    增加遍历文件夹函数

---
+++ installAllApk.py
@@ -0,0 +1,16 @@
+#!user/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+import CommonUtils
+
+if __name__ == '__main__':
+    fileList = CommonUtils.getAllFileFromCurrentDir("apk")
+    print fileList
+
+
+
+
+
+
